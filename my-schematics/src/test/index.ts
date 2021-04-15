3a7c87b9e34e5ca72b49d65b7d809035c9ffe9d5
aakansha-chandel <aakanshac.stacksummation@gmail.com>
2021-04-15 19:47:21 +0530

    my-schematic

---
+++ my-schematics/src/test/index.ts
@@ -0,0 +1,11 @@
+import { Schema } from './schema';
+import { Rule, SchematicContext, Tree } from '@angular-devkit/schematics';
+
+export function test(_options: Schema): Rule {
+  return (tree: Tree, _context: SchematicContext) => {
+    const name = _options.name;
+    console.log(name);
+    tree.create('test.ts' ,'Hello' + ' ' + name);
+    return tree;
+  };
+}
---
+++ projects/demo-app/src/app/app.module.ts
@@ -0,0 +1,53 @@
+import { BrowserModule } from '@angular/platform-browser';
+import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
+
+import { NgModule } from '@angular/core';
+
+import { AppRoutingModule } from './app-routing.module';
+import { AppComponent } from './app.component';
+import { NavPageComponent } from './nav-page/nav-page.component';
+import { LayoutModule } from '@angular/cdk/layout';
+import { MatToolbarModule } from '@angular/material/toolbar';
+import { MatButtonModule } from '@angular/material/button';
+import { MatSidenavModule } from '@angular/material/sidenav';
+import { MatIconModule } from '@angular/material/icon';
+import { MatListModule } from '@angular/material/list';
+import { AddFormComponent } from './add-form/add-form.component';
+import { MatInputModule } from '@angular/material/input';
+import { MatSelectModule } from '@angular/material/select';
+import { MatRadioModule } from '@angular/material/radio';
+import { MatCardModule } from '@angular/material/card';
+import { ReactiveFormsModule } from '@angular/forms';
+import { MatDashboardComponent } from './mat-dashboard/mat-dashboard.component';
+import { MatGridListModule } from '@angular/material/grid-list';
+import { MatMenuModule } from '@angular/material/menu';
+
+@NgModule({
+  declarations: [
+    AppComponent,
+    NavPageComponent,
+    AddFormComponent,
+    MatDashboardComponent
+  ],
+  imports: [
+    BrowserModule,
+    AppRoutingModule,
+    BrowserAnimationsModule,
+    LayoutModule,
+    MatToolbarModule,
+    MatButtonModule,
+    MatSidenavModule,
+    MatIconModule,
+    MatListModule,
+    MatInputModule,
+    MatSelectModule,
+    MatRadioModule,
+    MatCardModule,
+    ReactiveFormsModule,
+    MatGridListModule,
+    MatMenuModule
+  ],
+  providers: [],
+  bootstrap: [AppComponent]
+})
+export class AppModule { }
