f3b3aa54c089c6aea2179389f5c118f0f1d4bb0a
aakansha-chandel <aakanshac.stacksummation@gmail.com>
2021-04-15 19:50:13 +0530

    my-schematic code

--- my-schematics/src/test/index.ts
+++ my-schematics/src/test/index.ts
@@ -5,7 +5,7 @@ export function test(_options: Schema): Rule {
   return (tree: Tree, _context: SchematicContext) => {
     const name = _options.name;
     console.log(name);
-    tree.create('test.ts' ,'Hello' + ' ' + name);
+    tree.create('test.ts' ,'Demo' + ' ' + name);
     return tree;
   };
 }
